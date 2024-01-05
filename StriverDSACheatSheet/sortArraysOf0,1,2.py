a66f2c30647b4f352bb60f4e039f252a60e02920
suraj588 <surajbhansingh588@gmail.com>
2024-01-05 10:10:03 +0530

    adds problem description

--- StriverDSACheatSheet/sortArraysOf0,1,2.py
+++ StriverDSACheatSheet/sortArraysOf0,1,2.py
@@ -1,3 +1,6 @@
+# Given an array consisting of only 0s, 1s, and 2s.
+# Write a program to in-place sort the array without using inbuilt sort functions. ( Expected: Single pass-O(N) and constant space)
+
 class Solution:
     def sort_colors(self, nums) -> None:
       obj = {}
